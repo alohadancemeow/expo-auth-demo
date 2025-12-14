28c94033a4ef28dad830112be4439187d1fa38b0
Alohadancemeow <nawongsak@gmail.com>
2025-12-14 18:31:43 +0700

    feat: Implement Better Auth with Prisma and SQLite, including schema, API routes, and configuration.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,18 @@
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { betterAuth } from "better-auth";
 import { expo } from "@better-auth/expo";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/prisma/generated/prisma/client";
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const prisma = new PrismaClient();
+import path from 'path';
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL || `file:${path.join(process.cwd(), 'prisma/database.sqlite')}`
+})
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -14,7 +23,7 @@ export const auth = betterAuth({
   },
   plugins: [expo()],
   socialProviders: {},
-  trustedOrigins: ["exp://", "withbetterauth://"],
+  trustedOrigins: ["exp://", "withbetterauth://", "myapp://"],
   logger: {
     log: (level, message, ...args) => {
       console.log(`${level}: ${message}`);
